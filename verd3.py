2567fc82a0fa3af5b43378622016439cfa897989
Francisco Pérez <drfperez@gmail.com>
2023-06-21 08:41:55 +0200

    Create verd3.py

---
+++ verd3.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+
+def calcular_percentatge_verd(frame):
+    # Convertir el frame a espacio de color HSV
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Definir el rango de color verde en el espacio HSV
+    rang_verd_min = np.array([35, 100, 100])    # Valores mínimos para el verde en HSV
+    rang_verd_max = np.array([85, 255, 255])    # Valores máximos para el verde en HSV
+    
+    # Filtrar los píxeles que se encuentran dentro del rango verde
+    mask = cv2.inRange(frame_hsv, rang_verd_min, rang_verd_max)
+    
+    # Contar el número de píxeles verdes
+    pixels_verds = cv2.countNonZero(mask)
+    
+    # Calcular el porcentaje de color verde respecto al total
+    total_pixels = frame.shape[0] * frame.shape[1]
+    percentatge_verd = (pixels_verds / total_pixels) * 100
+    
+    return percentatge_verd, pixels_verds
+
+# Inicializar la lectura del vídeo
+video = cv2.VideoCapture('ruta_del_video.mp4')
+
+while True:
+    try:
+        # Leer el frame actual
+        ret, frame = video.read()
+        
+        # Salir del bucle si no hay más frames disponibles
+        if not ret:
+            break
+        
+        # Calcular el porcentaje de color verde y el número de píxeles verdes
+        percentatge_verd, pixels_verds = calcular_percentatge_verd(frame)
+        
+        # Mostrar los resultados
+        print(f"Percentatge verd: {percentatge_verd:.2f}%")
+        print(f"Píxels verds: {pixels_verds}")
+        
+        # Mostrar el frame con la máscara de color verde
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Mask", mask)
+        
+        # Esperar por la acción del usuario (presionar 'q' para salir)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    except Exception as e:
+        # Capturar y mostrar cualquier excepción ocurrida
+        print(f"Error: {str(e)}")
+        break
+
+# Liberar recursos
+video.release()
+cv2.destroyAllWindows()
+
